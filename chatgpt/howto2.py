2f86e4c666f2c3157f51ea0798cb7db5b3de86ac
tropkal <skratkon@gmail.com>
2023-11-27 09:44:56 +0100

    updated the chatgpt scripts to openai v1.0.0

--- chatgpt/howto2.py
+++ chatgpt/howto2.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-import openai
+from openai import OpenAI
+
+client = OpenAI(api_key=f.read().strip())
 import sys
 import os
 
@@ -8,18 +10,16 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 # !!!You need an API key!!!
 # https://platform.openai.com/account/api-keys
 with open(f"{dir_path}/.gpt_api_key.txt") as f:
-  openai.api_key = f.read().strip()
+  
 
 arg = ' '.join(sys.argv[1:])
 
-r = openai.ChatCompletion.create(
-  model="gpt-3.5-turbo",
-  #model="gpt-4",
-  messages=[
-        {"role": "system", "content": "You are ChatGPT, a large language model trained by OpenAI. Answer as concisely as possible."},
-        {"role": "user", "content": f"{arg}"}
-    ]
-)
+r = client.chat.completions.create(model="gpt-3.5-turbo",
+#model="gpt-4",
+messages=[
+      {"role": "system", "content": "You are ChatGPT, a large language model trained by OpenAI. Answer as concisely as possible."},
+      {"role": "user", "content": f"{arg}"}
+  ])
 
 text = r["choices"][0]["message"]["content"]
 if text.startswith('`') and text.endswith('`'):
