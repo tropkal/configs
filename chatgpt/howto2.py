43f7d2d6fadd55f407c32d204c6b1cff75d362cb
tropkal <skratkon@gmail.com>
2023-11-27 09:50:25 +0100

    latest version

--- chatgpt/howto2.py
+++ chatgpt/howto2.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 from openai import OpenAI
-
-client = OpenAI(api_key=f.read().strip())
 import sys
 import os
 
@@ -9,20 +7,22 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 
 # !!!You need an API key!!!
 # https://platform.openai.com/account/api-keys
-with open(f"{dir_path}/.gpt_api_key.txt") as f:
-  
+with open(f"{dir_path}/.gpt_api_key.txt", "r") as f:
+    api_key = f.readline().strip()
+    client = OpenAI(api_key=api_key)
 
-arg = ' '.join(sys.argv[1:])
+arg = """ """.join(sys.argv[1:])
 
 r = client.chat.completions.create(model="gpt-3.5-turbo",
-#model="gpt-4",
-messages=[
-      {"role": "system", "content": "You are ChatGPT, a large language model trained by OpenAI. Answer as concisely as possible."},
-      {"role": "user", "content": f"{arg}"}
-  ])
+    # model="gpt-4",
+    messages=[
+        {"role": "system", "content": "You are ChatGPT, a large language model trained by OpenAI. Answer as concisely as possible."},
+        {"role": "user", "content": f"{arg}"}
+    ]
+)
 
-text = r["choices"][0]["message"]["content"]
-if text.startswith('`') and text.endswith('`'):
-  text = text[1:-1]
+response = r.choices[0].message.content
+if response.startswith('`') and response.endswith('`'):
+    response = response[1:-1]
 
-print(text)
+print(response)
