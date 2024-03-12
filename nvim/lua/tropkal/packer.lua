f5f7d0000c8d6789c6d9789d10d9d5babd07155f
tropkal <skratkon@gmail.com>
2024-03-12 14:23:50 +0100

    small modification

--- nvim/lua/tropkal/packer.lua
+++ nvim/lua/tropkal/packer.lua
@@ -2,15 +2,16 @@
 
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
+vim.cmd('autocmd VimEnter * PackerSync')
 
 -- this had a return before `require`, dk why
 return require("packer").startup(function(use)
     -- Packer can manage itself
     -- this :packersync doesnt work, need to do it manually
-    use { "wbthomason/packer.nvim", run = ':PackerSync' }
+    use { "wbthomason/packer.nvim", run = { ':so', ':PackerSync' } }
 
     use {
-        "nvim-telescope/telescope.nvim", tag = "0.1.4",
+        "nvim-telescope/telescope.nvim", tag = "0.1.5",
         -- or                            , branch = "0.1.x",
         requires = { {"nvim-lua/plenary.nvim"} }
     }
@@ -40,8 +41,6 @@ return require("packer").startup(function(use)
         end
     }
 
-
-
     use("nvim-treesitter/playground")
     use("theprimeagen/harpoon")
     use("mbbill/undotree")
@@ -67,5 +66,4 @@ return require("packer").startup(function(use)
         {"rafamadriz/friendly-snippets"},
       }
     }
-
 end)
